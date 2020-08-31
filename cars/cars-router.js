8f284b88effcb72ec04046b8219b9c925b45a534
lateralcreativity <lateralcreativity@outlook.com>
2020-08-31 14:38:29 -0500

    CRUD functionality setup

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -24,6 +24,40 @@ router.get('/:id', (req, res) => {
     .catch(error => handleError(error, res));
 });
 
+router.post('/', (req, res) => {
+    const car = req.body;
+
+    if(!car.vin || !car.make || !car.model || !car.mileage) {
+        return res.status(400).json({ error: 'Vin, Make, Model and Mileage are required' });
+    } else {
+        Cars.create(car)
+        .then(payload => {
+            res.status(200).json(payload);
+        })
+        .catch(error => handleError(error, res))
+    }
+});
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+
+    Cars.update(changes, id)
+    .then(payload => {
+        res.status(200).json(payload);
+    })
+    .catch(error => handleError(error, res));
+});
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    Cars.remove(id)
+    .then(payload => {
+        res.status(200).json(payload);
+    })
+    .catch(error => handleError(error, res));
+})
+
 function handleError(error, res) {
     return res.status(500).json({ error: error });
 }
